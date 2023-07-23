96526fd94999f50fa15e56670b9a561725710206
Aditi <aditisingh7631@gmail.com>
2023-07-23 20:22:27 +0530

    first commit

---
+++ src/Aditi.js
@@ -0,0 +1,14 @@
+
+function Aditi() {
+    return (
+      <div className="App">
+      <h1>hello Aditi</h1>
+      </div>
+    );
+  }
+  
+  
+  export default Aditi;
+  
+  
+  
---
+++ src/index1.js
@@ -0,0 +1,4 @@
+import ReactDom from "react-dom";
+
+
+ReactDom.render(<h2>HELLO</h2>, document.getElementById("root"));
